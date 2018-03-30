e3c4bea4411c996d07bb862f35d25509ab1bba1c
Samuel Butta <samuel@butta.cz>
2018-03-30 09:12:47 +0200

    Add shapes validation

--- src/Data/Shapes.hs
+++ src/Data/Shapes.hs
@@ -22,9 +22,17 @@ class Validable a where
   valid = undefined
 
 -- TODO: complete instances for each type to check validity by `valid` function
-instance Validable Circle
-instance Validable Triangle
-instance Validable Quadrilateral
+instance Validable Circle where
+  valid (Circle ciRadius)= ciRadius > 0
+
+instance Validable Triangle where
+  valid (EquilateralTriangle a) =  a > 0
+  valid (IsoscelesTriangle itBase itLeg) = (itBase > 0) && (itLeg > 0) && (2*itLeg > itBase)
+  valid (ScaleneTriangle a b c) = (a > 0) && (b > 0) && (c > 0) && (a + b > c) && (a + c > b) && (c + b > a)
+
+instance Validable Quadrilateral where
+  valid (Square a) = a > 0
+  valid (Rectangle a b) = (a > 0) && (b > 0)
 
 -- TODO: create appropriate typeclass for 2D shapes (subclass of Validable)
 -- TODO: write instances for the types to compute circumference and area
